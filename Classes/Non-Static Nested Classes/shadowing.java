80c76f3e78be88a35a060b05565f16a76a5ffd6d
Sqap85 <148553397+Sqap85@users.noreply.github.com>
2024-10-17 09:47:07 +0300

    .

---
+++ Classes/Non-Static Nested Classes/shadowing.java
@@ -0,0 +1,26 @@
+public class ShadowTest {
+
+    public int x = 0;
+
+    class FirstLevel {
+
+        public int x = 1;
+
+        void methodInFirstLevel(int x) {
+            System.out.println("x = " + x);
+            System.out.println("this.x = " + this.x);
+            System.out.println("ShadowTest.this.x = " + ShadowTest.this.x);
+        }
+    }
+
+    public static void main(String... args) {
+        ShadowTest st = new ShadowTest();
+        ShadowTest.FirstLevel fl = st.new FirstLevel();
+        fl.methodInFirstLevel(23);
+    }
+}
+/*
+x (metot parametresi) → 23
+this.x (iç sınıftaki x değişkeni) → 1
+ShadowTest.this.x (dış sınıftaki x değişkeni) → 0
+*/

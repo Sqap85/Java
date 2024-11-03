3e491d2202d966d0f79fa4ab097521ec79c5a219
Sqap85 <148553397+Sqap85@users.noreply.github.com>
2024-11-03 14:31:16 +0300

    85

---
+++ toString/Main.java
@@ -0,0 +1,24 @@
+//toString() metodunu kullanarak nesnelerinizi daha anlamlı bir şekilde temsil edebilir ve debug sürecinde faydalı bilgiler elde edebilirsiniz.
+class Kitap {
+    private String ad;
+    private String yazar;
+    private int yil;
+
+    public Kitap(String ad, String yazar, int yil) {
+        this.ad = ad;
+        this.yazar = yazar;
+        this.yil = yil;
+    }
+
+    @Override
+    public String toString() {
+        return "Kitap Adı: " + ad + ", Yazar: " + yazar + ", Yıl: " + yil;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Kitap kitap = new Kitap("Küçük Prens", "Antoine de Saint-Exupéry", 1943);
+        System.out.println(kitap); // veya kitap.toString();
+    }
+}

ed32db49241ab26ee68b0d4a333a544a28568757
Sqap85 <148553397+Sqap85@users.noreply.github.com>
2024-11-26 16:55:10 +0300

    85

--- Polymorphism/(Run-Time) Polimorfizm (Metod Overriding)/Main.java
+++ Polymorphism/(Run-Time) Polimorfizm (Metod Overriding)/Main.java
@@ -10,11 +10,19 @@ class Dog extends Animal {
     void eat() {
         System.out.println("Dog is eating");
     }
+    
+    void bark(){
+        System.out.println("Hav! Hav!");
+    }
+    
 }
 
 public class Main {
     public static void main(String[] args) {
+      //Dog dog = new Animal(); Yapilamaz soldaki ust sinif olmak zorunda!
         Animal animal = new Dog(); // Polimorfizm
-        animal.eat();              // Dog sınıfındaki eat metodu çağrılır
+        animal.eat();// Dog sınıfındaki eat metodu çağrılır
+     // animal.bark Yapilamaz cunku animal classinda bark yok downcasting gerek
+        ((Dog) animal).bark();
     }
 }

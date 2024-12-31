c22002c3892d030185ca13d58b8dab12a4c691c6
Sqap85 <148553397+Sqap85@users.noreply.github.com>
2024-12-31 11:20:01 +0300

    85

--- Diary App/src/User.java
+++ Diary App/src/User.java
@@ -1,19 +1,26 @@
 import java.io.Serializable;
 
+// Kullanıcı bilgilerini tutmak için kullanılan User sınıfı
+// Bu sınıf Serializable arayüzünü implemente ediyor, böylece kullanıcı nesneleri serileştirilebilir
 public class User implements Serializable {
-    private String username;
-    private String password;
+    private String username; // Kullanıcının kullanıcı adı
+    private String password; // Kullanıcının şifresi
 
+    // User sınıfı için constructor (yapıcı metod)
+    // Yeni bir kullanıcı nesnesi oluştururken kullanıcı adı ve şifre belirtilir
     public User(String username, String password) {
-        this.username = username;
-        this.password = password;
+        this.username = username; // Kullanıcı adı atanıyor
+        this.password = password; // Şifre atanıyor
     }
 
+    // Kullanıcının kullanıcı adını döndürür
     public String getUsername() {
         return username;
     }
 
+    // Kullanıcı şifresinin doğru olup olmadığını kontrol eder
+    // Parametre olarak verilen şifre ile mevcut şifreyi karşılaştırır
     public boolean validatePassword(String password) {
-        return this.password.equals(password);
+        return this.password.equals(password); // Şifreler eşleşirse true döner
     }
 }
